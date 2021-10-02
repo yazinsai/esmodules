9ffd09e042b6e393aa2cf1e2e0faea31b4c17688
Yazin <github-7itbcg5@yaz.in>
2021-10-02 15:42:17 +0300

    What this? Imports work!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import moment from 'moment';
+import hi from './other';
+import { obj } from './alt';
 
 const logTime = () => {
   Logger.log(moment.now());
+  Logger.log(hi);
+  Logger.log(obj);
 }
